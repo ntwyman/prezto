86b69158f13d64d3792331709cd1317e362721b2
nick twyman <nick@conky.org>
2022-10-31 20:26:05 -0700

    Added brew completions and ponyup for pony management

--- modules/ntwyman/init.zsh
+++ modules/ntwyman/init.zsh
@@ -8,8 +8,8 @@ function prj {
 compctl -W $HOME/src -/ prj 
 
 function container {
-  project=${1:-seL4_play}
-  make -C /$HOME/src/sel4_dockerfiles user HOST_DIR=$HOME/src/${project}
+    project=${1:-seL4_play}
+    make -C /$HOME/src/sel4_dockerfiles user HOST_DIR=$HOME/src/${project}
 }
 
 #
@@ -23,23 +23,23 @@ alias brc='ssh bramble-control.local'
 alias br0='ssh bramble-worker0.local'
 alias br1='ssh bramble-worker1.local'
 
-# Add jp to pretty print json if we have jq 
-if [ `command -v jq` ]; then
+# Add jp to pretty print json if we have jq
+if [ $(command -v jq) ]; then
     alias jp="jq '.'"
 fi
 
 # VScode
 if [ -d /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin ]; then
-	alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
+    alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
 fi
 
 [ -f "${GHCUP_INSTALL_BASE_PREFIX:=$HOME}/.ghcup/env" ] && source "${GHCUP_INSTALL_BASE_PREFIX:=$HOME}/.ghcup/env"
 
-if [ `uname` = "Darwin" ]; then
+if [ $(uname) = "Darwin" ]; then
     ulimit -n 5000
 fi
 
-if [ `command -v direnv` ]; then
+if [ $(command -v direnv) ]; then
     eval "$(direnv hook zsh)"
 fi
 
@@ -51,10 +51,14 @@ if [ -d $HOME/bin ]; then
     path+=$HOME/bin
 fi
 
+if [ -d $HOME/.local/share/ponyup ]; then
+    path+=$HOME/.local/share/ponyup/bin
+fi
+
 setopt +o nomatch
-jdir=`ls -d /Applications/Julia* 2>/dev/null`
+jdir=$(ls -d /Applications/Julia* 2>/dev/null)
 setopt -o nomatch
-if [ -d $jdir/Contents/Resources/julia/bin ] ; then
+if [ -d $jdir/Contents/Resources/julia/bin ]; then
     alias julia="$jdir/Contents/Resources/julia/bin/julia"
 fi
 
@@ -63,7 +67,13 @@ export AWS_VAULT_KEYCHAIN_NAME=login
 export SDKMAN_DIR="/Users/nick/.sdkman"
 [[ -s "/Users/nick/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/nick/.sdkman/bin/sdkman-init.sh"
 
-
 [[ -s "$HOME/.nix-profile/etc/profile.d/nix.sh" ]] && source "$HOME/.nix-profile/etc/profile.d/nix.sh"
 
 [[ -d /opt/homebrew ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+
+    autoload -Uz compinit
+    compinit
+fi
