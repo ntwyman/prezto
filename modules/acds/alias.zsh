a40167080fcf2ccc7ac3ffb5e2606fdf54a2308b
nick twyman <nick.twyman@acds.gobal.com>
2025-04-01 14:58:52 +0100

    feat: tunnels for dev db and dev RabbitMQ

--- modules/acds/alias.zsh
+++ modules/acds/alias.zsh
@@ -1,2 +1,3 @@
 alias plocal='psql -h localhost -U acds_user -d postgres'
-alias tdev='ssh -N -L 5433:asm-db.cwjlttq8tphy.eu-west-2.rds.amazonaws.com:5432 asm:dev'
+alias tdevdb='ssh -N -L 5433:asm-db.cwjlttq8tphy.eu-west-2.rds.amazonaws.com:5432 asm:dev'
+alias tdevq='ssh -N -L 8443:b-b1adf345-8cc3-44b3-afd1-c6b296ba0561.mq.eu-west-2.amazonaws.com:443 asm:dev'
